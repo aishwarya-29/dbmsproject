bff08d35d8409830f19e35dbbc85496db25f43a0
unknown <aishwaryababu1212@gmail.com>
2020-09-03 22:58:42 +0530

    login and signup pages

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req,res){
+  res.render("users/login");
+});
+
+router.get("/register", function(req,res){
+  res.render("users/register");
 });
 
 module.exports = router;
